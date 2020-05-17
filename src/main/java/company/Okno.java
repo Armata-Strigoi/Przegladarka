857041938a2567997c7303a65f1c2ba865680610
DragonikongX <NoNameFB@wp.pl>
2020-05-17 22:43:35 +0200

    WebBarv2

--- src/main/java/company/Okno.java
+++ src/main/java/company/Okno.java
@@ -1,18 +1,47 @@
 package company;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
 
 public class Okno {
-    JFrame frame;
-    Container WebMap;
+    JFrame okno;
+    PasekAdresu pasekAdresu;
+    public static int numer = 0;
+    public String HistoriaAdresow[] = new String[1024];
     public Okno(){
-        frame = new JFrame("Przeglada");
-        frame.setSize(800,600);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        okno = new JFrame("WireFox");
+        okno.setSize(800,600);
+        okno.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        okno.setVisible(true);
+        okno.setExtendedState(JFrame.MAXIMIZED_BOTH);
 
-        PasekAdresu addressBar = new PasekAdresu(frame);
+       /* JTextField pole = new JTextField("spr");
+        okno.add(pole);
+        pole.setSize(okno.getWidth(),30);
+        pole.setLocation(0,100);
+        pole.setEditable(true);
+
+        KeyStroke alt = KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0);
+        pole.registerKeyboardAction(show,alt,JComponent.WHEN_FOCUSED);
+        */
+
+        pasekAdresu = new PasekAdresu(this);
+
     }
+    /*
+    ActionListener show = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent event){
+            for(int i=0;i<10;i++){
+                if(HistoriaAdresow[i]!=null)
+                    System.out.println(HistoriaAdresow[i]);
+            }
+            System.out.println("---");
+        }
+
+
+    };*/
 }
--- src/main/java/company/PasekAdresu.java
+++ src/main/java/company/PasekAdresu.java
@@ -1,32 +1,32 @@
 package company;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
 
 public class PasekAdresu {
-    public static int number = 0;
-    public String addressHistory[] = new String[1024];
     JTextField poleTekstowe;
 
-    public PasekAdresu(JFrame frame){
+    public PasekAdresu(Okno frame){
         poleTekstowe = new JTextField("");
-        frame.add(poleTekstowe);
-        poleTekstowe.setSize(frame.getWidth(),30);
+        frame.okno.add(poleTekstowe);
+        poleTekstowe.setSize(frame.okno.getWidth(),30);
         poleTekstowe.setEditable(true);
+
+        ActionListener EnterEvent = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event){
+                frame.HistoriaAdresow[frame.numer++] = poleTekstowe.getText();
+            }
+        };
 
         KeyStroke enter = KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0);
         poleTekstowe.registerKeyboardAction(EnterEvent,enter,JComponent.WHEN_FOCUSED);
-
     }
 
-    ActionListener EnterEvent = new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent event) {
-            addressHistory[number++] = poleTekstowe.getText();
-        }
-    };
+
 
 }
 
